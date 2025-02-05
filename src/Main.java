4914eded0066582936fb81184e5b504f116ff6e2
AVMihel <dok2000@list.ru>
2025-02-05 20:58:38 +0500

    one

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("БОНУС ОТ ОПЕРАТОРА");
+        int balance = 300;
+        int amount = 1_500;
+        int bonus = 0;
+        if (amount > 1000) {
+            bonus = amount / 100;
+        }
+        int result = balance + amount + bonus;
+        System.out.println("Ваш баланс: " + result);
     }
 }
